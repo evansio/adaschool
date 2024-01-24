38ccbb50bb4907e8963a7b5414efd6aed07649a3
Edgar Ortega <99567473+evansio@users.noreply.github.com>
2024-01-23 20:25:24 -0400

    Update procesando_informacion_en_bruto.py

--- Procesando información en bruto/procesando_informacion_en_bruto.py
+++ Procesando información en bruto/procesando_informacion_en_bruto.py
@@ -1,3 +1,11 @@
+"""Parte 4: Procesando información en bruto
+Imagina que no tuvieramos el acceso fácil de estos datos a través de la librería y tuvieramos que descargar los datos usando requests.
+
+Los datos son accesibles en esta dirección
+
+Realiza un GET request para descargarlos y escribe la respuesta como un archivo de texto plano con extensión csv (no necesitas pandas para esto, sólo manipulación de archivos nativa de Python)
+Agrupa el código para esto en una función reutilizable que reciba como argumento la url."""
+
 import requests
 
 def descargar_datos_csv(url):
